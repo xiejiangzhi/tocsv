1ef7102ae4510a6681aa81706db85b91ec3c75d1
jiangzhi.xie <xiejiangzhi@gmail.com>
2018-12-04 15:04:11 +0800

    INIT

---
+++ lib/tocsv.rb
@@ -0,0 +1,6 @@
+require "tocsv/version"
+
+module Tocsv
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/tocsv_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Tocsv do
+  it "has a version number" do
+    expect(Tocsv::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
